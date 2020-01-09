577e40c3c5f7529176cf85d1ffd0eb465dc105c8
Emmanuel <emmanuel.hakorimana@Emmanuels-MacBook-Pro.local>
2020-01-09 14:10:19 +0200

    Add a reducer

---
+++ index.js
@@ -0,0 +1,22 @@
+const BUY_CAKE = 'BUY_CAKE';
+
+function buyCake() {
+    return {
+        type: BUY_CAKE,
+        info:'First redux action'
+    }
+}
+
+const initialState = {
+    numOfCakes: 10
+}
+
+const reducer = (state = initialState, action) => {
+    switch(action.type){
+        case BUY_CAKE: return {
+            ...state,
+            numOfCakes: state.numOfCakes - 1
+        }
+        default: return state;
+    }
+}
