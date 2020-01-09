f96eee8ac53d084021f9a4c98fc1f2ae5abd6cab
Emmanuel HAKORIMANA <51243858+hakoemmy@users.noreply.github.com>
2020-01-09 15:02:56 +0200

    Add a reducer (#3)

--- index.js
+++ index.js
@@ -6,3 +6,17 @@ function buyCake() {
         info:'First redux action'
     }
 }
+
+const initialState = {
+    numOfCakes: 10
+}
+
+const reducer = (state = initialState, action) => {
+    switch(action.type){
+        case BUY_CAKE: return {
+            ...state,
+            numOfCakes: state.numOfCakes - 1
+        }
+        default: return state;
+    }
+}
