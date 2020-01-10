bc49ed3189d4bc221ef51aea647e9717029b783c
Emmanuel HAKORIMANA <51243858+hakoemmy@users.noreply.github.com>
2020-01-10 14:56:59 +0200

    Ft add middleware (#8)
    * Connect action, reducer and store
    
    *  Add mutiple reducers single technique
    
    * Add combineReducers
    
    * Add logger middleware

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const redux = require('redux');
+const reduxLogger = require('redux-logger');
+
 const createStore = redux.createStore;
 const combineReducers = redux.combineReducers;
+const applyMiddleware = redux.applyMiddleware;
+const logger = reduxLogger.createLogger();
 
 const BUY_CAKE = 'BUY_CAKE';
 const BUY_ICE_CREAM = 'BUY_ICE_CREAM';
@@ -69,9 +73,10 @@ const rootReducer = combineReducers({
     iceCream: iceCreamReducer
 })
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer,
+    applyMiddleware(logger));
 console.log('Initial State:', store.getState());
-const unsubscribe = store.subscribe(() => console.log('UpdatedState: ', store.getState()));
+const unsubscribe = store.subscribe(() => {});
 store.dispatch(buyCake());
 store.dispatch(buyCake());
 store.dispatch(buyCake());
